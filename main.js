47da69f789d9354f39394436356ae389e7f3d3d0
Andres Anni <andres.anni1985@gmail.com>
2022-11-16 09:33:39 -0300

    Clase 19 terminada. Fusion del menu mobile al clickear en el menu hamburguesa

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-let navEmail = document.getElementsByClassName("navbar-email")[0];
-let desktopMenu = document.getElementsByClassName("desktop-menu")[0]
+let menuEmail = document.getElementsByClassName("navbar-email")[0];
+let desktopMenu = document.getElementsByClassName("desktop-menu")[0];
+let mobileMenuIcon = document.querySelector(".menu");
+let mobileMenu = document.querySelector(".mobile-menu");
 
-navEmail.addEventListener("click",toggleDesktopMenu);
+menuEmail.addEventListener("click",toggleDesktopMenu);
+mobileMenuIcon.addEventListener("click",toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
 }
