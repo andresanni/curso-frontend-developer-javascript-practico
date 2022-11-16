63b06e3a604eab532ab27c343fdd3a2895e52404
Andres Anni <andres.anni1985@gmail.com>
2022-11-16 10:59:42 -0300

    Clase 20 terminada. Fusion del detalle de productos en el carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
-let menuEmail = document.getElementsByClassName("navbar-email")[0];
-let desktopMenu = document.getElementsByClassName("desktop-menu")[0];
-let mobileMenuIcon = document.querySelector(".menu");
-let mobileMenu = document.querySelector(".mobile-menu");
+const menuEmail = document.getElementsByClassName("navbar-email")[0];
+const desktopMenu = document.getElementsByClassName("desktop-menu")[0];
+const mobileMenuIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCartIcon = document.querySelector(".navbar-shopping-cart");
+const asideProductDetail = document.querySelector(".product-detail");
+
 
 menuEmail.addEventListener("click",toggleDesktopMenu);
 mobileMenuIcon.addEventListener("click",toggleMobileMenu);
+menuCartIcon.addEventListener("click",toggleCartDetail);
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle("inactive")
+    const isAsideProductDetailOpened = !asideProductDetail.classList.contains("inactive");
+
+    if(isAsideProductDetailOpened)
+    {
+        asideProductDetail.classList.add("inactive");
+    }
+    
+    desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu(){
+    const isAsideProductDetailOpened = !asideProductDetail.classList.contains("inactive");
+    
+    if(isAsideProductDetailOpened)
+    {
+        asideProductDetail.classList.add("inactive");
+    }
     mobileMenu.classList.toggle("inactive");
 }
+
+function toggleCartDetail(){
+    const isMobileMenuOpen = !mobileMenu.classList.contains("inactive");
+
+    if(isMobileMenuOpen){
+    mobileMenu.classList.add("inactive");
+   }
+    
+   asideProductDetail.classList.toggle("inactive");
+}
+
